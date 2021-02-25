f539201c448c219e931f444e51b5c3524dc0a542
Matheus Castro <49662686+Couks@users.noreply.github.com>
2021-02-25 18:25:23 -0300

    Criando context

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,31 @@
-import Head from "next/head";
 
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/CountDown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import { ChallengeBox } from "../components/ChallengeBox";
 
+import Head from 'next/Head';
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+
+      <Head>
+        <title>Início | move.It</title>
+      </Head>
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
-
+          <CompletedChallenges />
+          <Countdown/>
         </div>
-        <div>
 
+        <div>
+          <ChallengeBox />
         </div>
       </section>
     </div>
