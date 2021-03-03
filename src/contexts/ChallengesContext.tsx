b754cb86035082ef34fa468bf6f617cd7f745c26
Matheus Castro <49662686+Couks@users.noreply.github.com>
2021-03-03 17:50:52 -0300

    refresh

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,5 +1,7 @@
 import React, { ReactNode, createContext, useState, useEffect } from "react";
+import Cookies from 'js-cookie';
 import challenges from '../../challenges.json';
+
 
 interface Challenge {
   type: 'body' | 'eye';
@@ -21,16 +23,18 @@ interface ChallengesContextData {
 
 interface ChallengesProviderProps {
   children: ReactNode;
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number
 }
 
 export const ChallengesContext = createContext({} as ChallengesContextData);
 
-
-
-export function ChallengesProvider({ children }: ChallengesProviderProps) {
-  const [level, setLevel] = useState(1);
-  const [currentExperience, setCurrentExperience] = useState(0);
-  const [challengesCompleted, setChallengesCompleted] = useState(0);
+export function ChallengesProvider({ children, ...rest } : ChallengesProviderProps) {
+  
+  const [level, setLevel] = useState(rest.level ?? 1);
+  const [currentExperience, setCurrentExperience] = useState(rest.currentExperience ?? 0);
+  const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted?? 0);
 
   const [activeChallenge, setActiveChallenge] = useState(null);
 
@@ -39,6 +43,13 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
   useEffect(() => {
     Notification.requestPermission();
   },[])
+
+  useEffect(() => {
+    Cookies.set('level', String(level));
+    Cookies.set('currentExperience', String(currentExperience));
+    Cookies.set('challengesCompleted', String(challengesCompleted));
+
+  },[level, currentExperience, challengesCompleted]);
 
   function levelUp() {
     setLevel(level + 1);
---
+++ t
@@ -0,0 +1,29 @@
+[33mcommit fbcdba5fcee8e72bc5cfe5c242d14b5802da7594[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m)[m
+Author: Matheus Castro <49662686+Couks@users.noreply.github.com>
+Date:   Wed Mar 3 16:18:17 2021 -0300
+
+    finalizando o projeto raiz
+
+[33mcommit 413a3a843f10de5616437df8888c0f3d2753808d[m
+Author: Matheus Castro <49662686+Couks@users.noreply.github.com>
+Date:   Fri Feb 26 18:59:43 2021 -0300
+
+    Countdown Context feito
+
+[33mcommit 722a3b42361d16f106309300202aefa22f35366c[m
+Author: Matheus Castro <49662686+Couks@users.noreply.github.com>
+Date:   Fri Feb 26 15:12:06 2021 -0300
+
+    criando context
+
+[33mcommit f539201c448c219e931f444e51b5c3524dc0a542[m
+Author: Matheus Castro <49662686+Couks@users.noreply.github.com>
+Date:   Thu Feb 25 18:25:23 2021 -0300
+
+    Criando context
+
+[33mcommit 284a9d80ab7cb5e74d8d593bca3961b7d910061b[m
+Author: Matheus Castro <49662686+Couks@users.noreply.github.com>
+Date:   Thu Feb 25 12:01:42 2021 -0300
+
+    first commit
